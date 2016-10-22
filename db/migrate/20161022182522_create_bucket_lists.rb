35dbe093c73910752cd592543833fc0ea71637da
Hannah <hannah.masila@andela.com>
2016-10-22 21:31:18 +0300

    ft(models): generate the bucket_list model

---
+++ db/migrate/20161022182522_create_bucket_lists.rb
@@ -0,0 +1,10 @@
+class CreateBucketLists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bucket_lists do |t|
+      t.string :name
+      t.references :user, colum: :created_by, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
