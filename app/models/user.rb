fb785f20ef89e18f64c4dc34ac5571ee5d614742
Hannah <hannah.masila@andela.com>
2016-10-28 12:56:10 +0300

    ft(models): add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_many :bucket_lists
+  has_secure_password
+
+  VALID_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  validates :name, presence: true
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: VALID_EMAIL }
 end
