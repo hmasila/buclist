fc207ae3e6c1071cecde649881f668f672415dfb
Hannah <hannah.masila@andela.com>
2016-10-28 13:13:28 +0300

    bg(models): rename  to  and

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_many :bucket_lists
+  has_many :tokens
+
   has_secure_password
 
   VALID_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
-  validates :name, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
   validates :email,
             presence: true,
             uniqueness: true,
