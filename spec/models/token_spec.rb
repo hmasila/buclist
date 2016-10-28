e11797a988887e15c974aadfaea8df88d5b168d9
Hannah <hannah.masila@andela.com>
2016-10-28 12:40:19 +0300

    ft(specs): create specs for the token model

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Token, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:token) }
+  it { should validate_uniqueness_of(:token) }
+  it { should belong_to(:user) }
 end
