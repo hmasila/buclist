de5f02e9996eab7502fe63243db37b313284457c
Hannah <hannah.masila@andela.com>
2016-10-29 10:22:40 +0300

    add scope for search and pagination

--- app/models/bucket_list.rb
+++ app/models/bucket_list.rb
@@ -1,3 +1,12 @@
 class BucketList < ApplicationRecord
   belongs_to :user
+  has_many :items
+
+  validates :name, presence: true
+
+  scope :search, -> (name) { where('name ILIKE ?', "%#{name}%") }
+
+  scope :paginate, lambda do |parameters|
+    limit(parameters[:limit]).offset(parameters[:offset])
+  end
 end
