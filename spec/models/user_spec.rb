8adc656479794e2592e1fe335f684fe61ed7dcc9
Hannah <hannah.masila@andela.com>
2016-10-31 11:31:09 +0300

    ft(users): validate presence of password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :password }
   it { should have_secure_password }
-  it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(5) }
 
   it { should validate_presence_of :email }
