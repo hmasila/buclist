278e2a4a4130d5744277031232b2c2053a800690
Hannah <hannah.masila@andela.com>
2016-11-02 02:54:00 +0300

    ft(specs): create shared examples

---
+++ spec/support/shared_examples/json_response.rb
@@ -0,0 +1,9 @@
+RSpec.shared_examples "a http response" do |status, message = nil|
+  it "returns a status code #{status}" do
+    expect(response).to have_http_status(status)
+  end
+
+  it "returns a #{status} message" do
+    expect(json["message"]).to match(message)
+  end if message
+end
