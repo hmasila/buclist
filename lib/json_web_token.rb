892e0f92b4ac0383a581ef51f1998be746d52e95
Hannah <hannah.masila@andela.com>
2016-11-02 01:38:15 +0300

    ft(specs): write test for index action

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,12 +1,14 @@
-class JsonWebToken
-  def self.encode(payload, expiration = 1.hour.from_now)
-    payload[:exp] = expiration.to_i
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
-  end
+module Api
+  class JsonWebToken
+    def self.encode(payload, expiration = 1.hour.from_now)
+      payload[:exp] = expiration.to_i
+      JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    end
 
-  def self.decode(token)
-    JWT.decode(token, Rails.application.secrets.secret_key_base).first
-  rescue
-    nil
+    def self.decode(token)
+      JWT.decode(token, Rails.application.secrets.secret_key_base).first
+    rescue
+      nil
+    end
   end
 end
