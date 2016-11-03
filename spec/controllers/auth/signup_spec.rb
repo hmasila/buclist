0441e8105c07735ee7e4f9792b64cb69581fd0b8
Hannah <hannah.masila@andela.com>
2016-11-03 12:20:36 +0300

    ft(spec): create login spec in auth folder

--- spec/controllers/auth/signup_spec.rb
+++ spec/controllers/auth/signup_spec.rb
@@ -6,32 +6,32 @@ RSpec.describe "User requstration", type: :request do
   let!(:existing_user) { create(:user) }
 
   describe "POST /signup" do
-  let!(:req) { post "/signup", params, headers }
+    let!(:req) { post "/signup", params, headers }
 
-  context "when a new user signs up" do
-    it_behaves_like "a http response", 201, /Account created successfully/
-  end
-
-  context "when and existing user signs up" do
-    let(:params) do
-      {
-        email: existing_user.email,
-        password: existing_user.password
-      }
+    context "when a new user signs up" do
+      it_behaves_like "a http response", 201, /Account created successfully/
     end
 
-    it_behaves_like "a http response", 422, /User already exists/
-  end
+    context "when and existing user signs up" do
+      let(:params) do
+        {
+          email: existing_user.email,
+          password: existing_user.password
+        }
+      end
 
-  context "when a new user signs up with the invalid parameters" do
-    let(:params) do
-      {
-        email: Faker::Lorem.word,
-        password: nil
-      }
+      it_behaves_like "a http response", 422, /User already exists/
     end
 
-    it_behaves_like "a http response", 422, /Account could not be created/
+    context "when a new user signs up with the invalid parameters" do
+      let(:params) do
+        {
+          email: Faker::Lorem.word,
+          password: nil
+        }
+      end
+
+      it_behaves_like "a http response", 422, /Account could not be created/
+    end
   end
-end
 end
