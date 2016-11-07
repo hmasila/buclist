3b6a453f84ce50ad62315cf590fca6c98ce3ea51
Hannah <hannah.masila@andela.com>
2016-11-07 15:55:27 +0300

    ft(serializers): create items serializer

--- app/serializers/bucket_list_serializer.rb
+++ app/serializers/bucket_list_serializer.rb
@@ -1,4 +1,5 @@
 class BucketListSerializer < ActiveModel::Serializer
+  include DateFormat
   attributes :id, :name, :items, :date_created, :date_modified, :created_by
   has_many :items
 
---
+++ app/serializers/concerns/date_fomart.rb
@@ -0,0 +1,9 @@
+module DateFormat
+  def date_created
+    object.created_at.strftime("%Y-%m-%d %l:%M:%S")
+  end
+
+  def date_modified
+    object.updated_at.strftime("%Y-%m-%d %l:%M:%S")
+  end
+end
