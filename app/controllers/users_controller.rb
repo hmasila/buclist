c36f07e720b8a3f423c3b93510950103f4f78dbc
Hannah <hannah.masila@andela.com>
2016-11-13 19:37:44 +0300

    ft(docs): generate the api documentation with aglio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       auth_token = AuthenticateUser.new(user.email, user.password).call
-      response = { message: Messages.signup_successful, auth_token: auth_token }
+      response = { message: Messages.signup_successful,
+                   auth_token: auth_token.result }
       json_response(response, :created)
     else
       json_response({ message: Messages.signup_failed }, :unprocessable_entity)
